45773f55a610675d27efe827704669ba1428cc13
Daesiker <eotlr680@naver.com>
2022-08-18 14:13:18 +0900

    08/18

--- baek11659.swift
+++ baek11659.swift
@@ -3,12 +3,28 @@ import Foundation
 let nm = readLine()!.split(separator: " ").map { Int(String($0))! }
 
 let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+var preSum:[Int] = []
+
+for i in 0..<arr.count {
+    if i == 0 {
+        preSum.append(arr[i])
+    } else {
+        let value = arr[i] + preSum[i-1]
+        preSum.append(value)
+    }
+}
+
 
 for _ in 0..<nm[1] {
-    let ij = readLine()!.split(separator: " ").map { Int(String($0))! }
-    var answer = 0
-    for i in ij[0]-1...ij[1]-1 {
-        answer += arr[i]
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let x = input[0]-1
+    let y = input[1]-1
+
+    if x == 0 {
+        print(preSum[y])
+    } else if x == y{
+        print(arr[x])
+    } else {
+        print(preSum[y] - preSum[x-1])
     }
-    print(answer)
 }
