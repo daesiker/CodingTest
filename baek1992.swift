f98fc5374921cc88a61171e54988120e76a355cc
Daesiker <eotlr680@naver.com>
2022-09-02 11:22:00 +0900

    09/02

--- baek1992.swift
+++ baek1992.swift
@@ -5,12 +5,16 @@ let n = Int(readLine()!)!
 var array:[[Int]] = []
 
 for _ in 0..<n {
-    let tmp = readLine()!.split(separator: " ").map { Int(String($0))! }
-    array.append(tmp)
+    let tmp = readLine()!
+    var tmp2 = [Int]()
+    for i in tmp {
+        tmp2.append(Int(String(i))!)
+    }
+
+    array.append(tmp2)
 }
 
-var blue = 0
-var white = 0
+var answer = ""
 
 func check(_ r: Int, _ c: Int, _ sz: Int) -> Void {
 
@@ -18,27 +22,25 @@ func check(_ r: Int, _ c: Int, _ sz: Int) -> Void {
 
     for i in r..<r+sz {
         for j in c..<c+sz {
-            if array[i][j] != value {
+            if value != array[i][j] {
+                answer += "("
                 check(r, c, sz/2)
-                check(r+(sz/2), c, sz/2)
                 check(r, c+(sz/2), sz/2)
+                check(r+(sz/2),c, sz/2)
                 check(r+(sz/2), c+(sz/2), sz/2)
-
+                answer += ")"
                 return
             }
         }
     }
 
     if value == 1 {
-        blue += 1
+        answer += "1"
     } else {
-        white += 1
+        answer += "0"
     }
 
 }
 
-
-
 check(0, 0, n)
-print(white)
-print(blue)
+print(answer)
