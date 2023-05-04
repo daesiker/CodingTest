0b5afbe52a113fc67ba5cf210286da6aab01ac22
Daesiker <eotlr680@naver.com>
2023-05-04 16:24:44 +0900

    5/4

--- baek11728.swift
+++ baek11728.swift
@@ -1,8 +1,30 @@
 import Foundation
 
-let nm = readLine()!.split(separator: " ").map { Int($0)! }
-let arr1 = readLine()!.split(separator: " ").map { String($0) }
-let arr2 = readLine()!.split(separator: " ").map { String($0) }
-let answer = Array(Set(arr1 + arr2)).sorted()
+let input = readLine()!.components(separatedBy: " ").map { Int($0)! }
+let (n, m) = (input[0], input[1])
 
-print(answer.joined(separator: " "))
+let arr1 = readLine()!.components(separatedBy: " ").map { Int($0)! }
+let arr2 = readLine()!.components(separatedBy: " ").map { Int($0)! }
+
+var i = 0
+var j = 0
+var answer = ""
+while i < n || j < m {
+    if i == n {
+        answer += "\(arr2[j]) "
+        j += 1
+    } else if j == m {
+        answer += "\(arr1[i]) "
+        i += 1
+    }else if arr1[i] > arr2[j] {
+        answer += "\(arr2[j]) "
+        j += 1
+    } else {
+        answer += "\(arr1[i]) "
+        i += 1
+    }
+
+
+}
+
+print(answer)
---
+++ baek2776.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+let T = Int(readLine()!)!
+
+
+for _ in 0..<T {
+    let n = Int(readLine()!)!
+    let arr1 = readLine()!.components(separatedBy: " ").map { Int($0)! }.sorted(by: <)
+    let _ = Int(readLine()!)!
+    let arr2 = readLine()!.components(separatedBy: " ").map { Int($0)! }
+
+    for target in arr2 {
+
+        var start = 0
+        var end = n-1
+        var result = -1
+        while start <= end {
+            let mid = (start+end)/2
+
+            if arr1[mid] > target {
+                end = mid - 1
+            } else {
+                result = mid
+                start = mid + 1
+            }
+
+        }
+
+        if result != -1 && arr1[result] == target {
+            print(1)
+        } else {
+            print(0)
+        }
+
+    }
+
+}
